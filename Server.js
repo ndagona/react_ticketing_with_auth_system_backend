df5aeadc6286cad8d4a3c4dd4531be521d2d467a
esige_temp <Esige Ndagona@WATUKE.WATUAFRICA.CO.KE>
2025-02-20 15:16:08 +0300

    Auth Complete

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const database = require("better-sqlite3")("Dave.db")
 database.pragma("journal_mode = WAL")
 const cors = require("cors")
 const jwt = require("jsonwebtoken")
-
+const cookieParser = require("cookie-parser");
 //App declaration
 
 const app = express()
@@ -12,6 +12,7 @@ const app = express()
 //Use
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(cookieParser());
 app.use(cors(
 
     {
@@ -143,7 +144,7 @@ app.post('/register', (req, res) => {
             sameSite: "strict",
             maxAge: 1000 * 60 * 60 * 24 //Cookie be good, one day
         })
-        res.json({ message: "Signup successful", entry: true });
+        res.json({ message: "Signup successful", state: true });
     } else {
         res.json({ message: writeRes.toString() });
     }
@@ -152,10 +153,6 @@ app.post('/register', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    console.log("login post request")
-    console.log(req.body)
-
-    console.log(req.body)
     if (typeof req.body.username !== "string" || req.body.username.length < 3 || req.body.username.length > 20) {
         return res.json({ message: "Invalid Username" })
 
@@ -194,6 +191,47 @@ app.post('/login', (req, res) => {
         maxAge: 1000 * 60 * 60 * 24 //Cookie be good, one day
     })
     return res.send(res_dict)
+})
+
+
+//Logged in API
+const verify_user = (req, res, next) => {
+    const OurSimpleApp = req.cookies.OurSimpleApp;
+    if (!OurSimpleApp) {
+        console.log("In ender statement")
+        return res.send({
+            message: "Not logged in",
+            state: false
+        })
+    }
+
+    else {
+        console.log("In cookie else")
+        jwt.verify(
+            OurSimpleApp, process.env.JWTTOKENKEY, (err, deku) => {
+                if (err) {
+                    return res.send({
+                        message: "Not logged in",
+                        username: "",
+                        email: "",
+                        state: false
+                    })
+                } else {
+                    req.username = deku.username;
+                    req.email = deku.email;
+                    next()
+                }
+            }
+        )
+    }
+}
+
+app.get("/loggedin", verify_user, (req, res) => {
+    return res.json({
+        username: req.username,
+        email: req.email,
+        state: true
+    })
 })
 
 //End of endPoints
